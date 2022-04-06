ea022e5af7e61ba941b0b551129932da4fe8b9f6
Chinh Do <do.inby@gmail.com>
2022-04-06 23:56:39 +1000

    Add Header and Hero sections

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import logo from '../images/logo.svg';
+
+export default function Header() {
+  return (
+    <header class='container'>
+      <img src={logo} alt="Maker's Logo" />
+    </header>
+  );
+}
---
+++ src/Components/Hero.js
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <section id='hero' class='text-center'>
+      <h1>
+        Get paid for the work you <span class='text-primary'>love</span> to do.
+      </h1>
+      <p>
+        The 9-5 grind is so last century. We believe in living life on your own
+        terms. Whether you’re looking to escape the rat race or set up a side
+        hustle, we’ve got you covered.
+      </p>
+    </section>
+  );
+}

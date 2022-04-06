81f6af5be4af88b90b945c05dcbd688dbba07fca
Chinh Do <do.inby@gmail.com>
2022-04-06 23:56:17 +1000

    Install dependencies and configure theme

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  darkMode: true,
+  theme: {
+    colors: {
+      primary: '#3EE9E5',
+      secondary: '#093F68',
+      black: '#080C20',
+      white: '#FFFFFF',
+      gray: '#777F98',
+      error: '#FF2965',
+    },
+    fontFamily: {
+      body: ['Manrope', 'sans-serif'],
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
